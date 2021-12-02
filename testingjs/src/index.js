fc20cf829a20f414a42177d41bb928fb120fc787
CGW-G1 <91236669+CGW-G1@users.noreply.github.com>
2021-12-02 11:12:07 +0800

    audio ok

---
+++ testingjs/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Testing from './testing/test';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Testing />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
---
+++ testingjs/src/testing/test.js
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import API from '../API';
+
+function Testing() {
+
+    const [textSubmitting, settextSubmitting] = useState("");
+    const [rcvAudio, setrcvAudio] = useState("");    
+
+    function handleInputChange (e) {
+        settextSubmitting(e.target.value);
+    }
+
+    async function handleSubmit (e) {
+        e.preventDefault();
+        console.log('Submitting text: ', textSubmitting);
+
+        if (textSubmitting === '') {
+            alert('string empty!');
+            return;
+        } // dont waste GET attempt with empty string, daily 350 limit
+        
+        if (textSubmitting.length > 49000) {
+            alert('string too long!');
+            return;
+        } // dont waste GET attempt with string exceed 100kb limit
+
+        let apikey = "52b16d4b4f1246ee800ea25d1b9fe536";
+        let apilang = "en-us";
+        let apicodec = "MP3";
+        let apiformat = "8khz_8bit_mono"; // default is 8khz_8bit_mono
+        let apiB64Status = true; // default is false, true for output as Base64 string format
+
+        const response = await API.get(`/?key=${apikey}&hl=${apilang}&c=${apicodec}&f=${apiformat}&src=${textSubmitting}&b64=${apiB64Status}`);
+
+        if (response.status === 200) {
+            console.log('200 ok');
+            console.log('Response: ', response);
+            console.log('Response.data: ', response.data);
+
+            setrcvAudio(response.data);
+            
+            // setrcvAudio("https://www.learningcontainer.com/wp-content/uploads/2020/02/Kalimba.mp3");
+
+            // let audioReturn = new Audio(response.data);
+            // audioReturn.play();
+
+        } else {
+            console.log('error: ',response.status);
+            return;
+            }
+    }
+
+    const playAudio = () => {
+
+        if (rcvAudio==="") {
+            return (
+                <p>no audio currently</p>
+            )
+        }      
+
+        return (
+            <div>
+            <p>have audio</p>
+            <audio id="audioPlayer" src={rcvAudio} autoPlay="autoplay">audio not supported</audio>
+            </div>
+        )
+
+    }
+
+    return (
+        <div className='canvass'>
+            <div className='displayWindow'>
+                <form onSubmit={handleSubmit}>
+                    <h3>Input text for speech return</h3>
+                    <input type='text' placeholder='Punctuation/numerals ok.' onChange={handleInputChange}></input>                    
+                    <input type='submit' value='Submit' />
+                </form>
+                {playAudio(rcvAudio)}
+            </div>
+        </div>
+    )
+}
+
+export default Testing;
